916b6c9f6f4f683852d2e2c04bec28a5603c132b
rubi <naiman.music.info@gmail.com>
2023-02-16 16:01:13 +0100

    added removal of host in dict after unban, added auto absolute path of Fail2Ban.py and installer script

--- src/Fail2Ban.py
+++ src/Fail2Ban.py
@@ -4,11 +4,13 @@ from FileHandling import *
 from TimeHandling import *
 from XmlHandling import *
 import logging
+from os import path
 
 SSHLOGS = "C:/ProgramData/ssh/logs/sshd.log"
 FailedLoginLimit = 3
 FailedLoginTime = 60        # seconds
 BanDuration = 90         # seconds
+script_path = path.dirname(path.abspath(__file__))
 logging.basicConfig(filename='logs\Fail2Ban.log', encoding='utf-8', level=logging.DEBUG)
 
 def GetFailedHosts(FailedLines: list, MaxLogonAttemps: int) -> dict:
@@ -34,7 +36,8 @@ def CheckBanAge(dict: dict):
         if FreeDate <= GetDate():
             UnbannedHost = Host(host)
             UnbannedHost.UnbanIP()
-            logging.debug(f"{host} unbanned")
+            del dict[host]
+            logging.debug(f"{host} unbanned and removed from sshjail")
 
 async def main():
     logging.info("Fail2Ban service started")
@@ -66,11 +69,8 @@ async def main():
                 logging.debug(f"{host} banned")
                 SSHJail[host] = GetFreeDate(BanDuration)
 
+        DictToXml(SSHJail, f'{script_path}\SSHJail.xml')
         CheckBanAge(SSHJail)
-        DictToXml(SSHJail, 'SSHJail.xml')
-        logging.debug(f'SSHJail Content: {SSHJail}')
-
-    logging.info("Fail2Ban service stopped")
 
 if __name__ == "__main__":
     asyncio.run(main())
