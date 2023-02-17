600b47665e71d544fac7196f595e62a3e783c290
rubi <naiman.music.info@gmail.com>
2023-02-17 14:44:19 +0100

    added sshd_config to installer

--- installer.py
+++ installer.py
@@ -4,6 +4,7 @@ import zipfile
 from os import path, system, chdir
 from subprocess import run
 from sys import exec_prefix
+import fileinput
 try:
     import wget
 except:
@@ -12,6 +13,23 @@ except:
 
 mainpath = path.dirname(path.abspath(__name__))
 chdir(mainpath)
+
+try:
+    # Specify the filename and the lines to search and replace
+    filename = "C:\\ProgramData\\ssh\\sshd_config"
+    search_lines = ["#SyslogFacility AUTH", "#LogLevel INFO"]
+    replace_lines = ["SyslogFacility = LOCAL0", "LogLevel = Debug3"]
+
+    # Use the fileinput module to modify the lines in-place
+    with fileinput.FileInput(filename, inplace=True, backup=".bak") as file:
+        for line in file:
+            if line.strip() in search_lines:
+                index = search_lines.index(line.strip())
+                line = replace_lines[index] + "\n"
+            print(line, end="")
+except Exception as e:
+    print(e)
+    print("Please configure sshd_config manually")
 
 pathtomain = f"src\\Fail2Ban.py"
 pythonpath = exec_prefix
@@ -27,12 +45,14 @@ if path.exists(f"{NssmName}") == False:
     except Exception as e:
         print(e)
         print(f"Error downloading from {DownloadNSSM}")
+        exit()
 
     try:
         with zipfile.ZipFile(f"{NssmName}.zip", 'r') as zip_ref:
             zip_ref.extractall()
     except Exception as e:
         print(e)
+        exit()
 
 try:
     run(
